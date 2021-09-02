eb740243b4e67a9134554077dd4a5353f940b1df
Gabriel C. Cunha <gcoelho-@student.42sp.org.br>
2021-09-02 15:23:05 -0300

    Implemented strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -1,28 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                         :::      ::::::::  */
-/*  ft_strrchr.c                                         :+:      :+:    :+:  */
+/*  ft_strncmp.c                                         :+:      :+:    :+:  */
 /*                                                     +:+ +:+         +:+    */
 /*  By: gcoelho- <gcoelho-@student.42sp.org.br>      +#+  +:+       +#+       */
 /*                                                 +#+#+#+#+#+   +#+          */
-/*  Created: 2021/09/02 10:20:41 by gcoelho-            #+#    #+#            */
-/*  Updated: 2021/09/02 14:57:50 by gcoelho-           ###   ########.fr      */
+/*  Created: 2021/09/02 14:58:37 by gcoelho-            #+#    #+#            */
+/*  Updated: 2021/09/02 15:06:20 by gcoelho-           ###   ########.fr      */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strrchr(const char *s, int c)
+int	ft_strncmp(const char *s1, const char *s2, size_t len)
 {
 	size_t	idx;
 
-	idx = ft_strlen(s) + 1;
-	while (--idx >= 0)
-	{
-		if (s[idx] == c)
-			return ((char *) &s[idx]);
-		if (idx == 0)
-			break ;
-	}
-	return (NULL);
+	idx = -1;
+	while (++idx < len && s1[idx] && s2[idx])
+		if (s1[idx] != s2[idx])
+			return ((unsigned char) s1[idx]
+				- (unsigned char) s2[idx]);
+	return (0);
 }
